7ca9415f71a16ba3408443887513a792adf6a432
lalukumar <lalu07442@gmail.com>
2021-08-26 12:06:30 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyBUqN8w3-RXwGmWpNW5csEtXFSHn4O5wAs",
+    authDomain: "covid19t-8d671.firebaseapp.com",
+    projectId: "covid19t-8d671",
+    storageBucket: "covid19t-8d671.appspot.com",
+    messagingSenderId: "648284298168",
+    appId: "1:648284298168:web:dcc2d83f9d0ddb0f7d0d4d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
